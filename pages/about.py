58afee316f3c2392aba43a509723885af88c0486
kos1nys <mrshark854@gmail.com>
2025-04-17 00:54:49 +0300

    1

--- pages/about.py
+++ pages/about.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
+with open("storage/images/i.webp", "rb") as f:
+    st.download_button("Скачать косинуса", f)
+
 st.badge("В разработке", icon=":material/code:", color="gray")
