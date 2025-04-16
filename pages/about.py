6e3505a21c0e6fc7905fdc53e629ac7e7ac238e3
kos1nys <mrshark854@gmail.com>
2025-04-17 02:22:17 +0300

    1

--- pages/about.py
+++ pages/about.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-with open("storage/images/i.webp", "rb") as f:
-    st.download_button("Скачать косинуса", f, "i.webp")
+with st.expander("Описание"):
+    st.write("""
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint atque labore enim, iusto repellat assumenda ut dolorum? Minus, officiis natus, aperiam voluptatibus mollitia doloribus pariatur, dignissimos quia nesciunt dolorum provident voluptates molest
+""")
+    with open("storage/images/i.webp", "rb") as f:
+        st.download_button("Скачать косинуса", f, "i.webp")
+
+
 
 st.badge("В разработке", icon=":material/code:", color="gray")
