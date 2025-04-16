9a1b27849c17f82df5ea9bff441cb15bbfc035eb
kos1nys <mrshark854@gmail.com>
2025-04-17 02:24:55 +0300

    1

--- pages/about.py
+++ pages/about.py
@@ -1,12 +1,17 @@
 import streamlit as st
 
-with st.expander("Описание"):
-    st.write("""
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint atque labore enim, iusto repellat assumenda ut dolorum? Minus, officiis natus, aperiam voluptatibus mollitia doloribus pariatur, dignissimos quia nesciunt dolorum provident voluptates molest
-""")
-    with open("storage/images/i.webp", "rb") as f:
-        st.download_button("Скачать косинуса", f, "i.webp")
+dataTab, memeTab = st.tabs(["Данные", "Мемы"])
 
+with dataTab:
+    with st.expander("Описание"):
+        st.write("""
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint atque labore enim, iusto repellat assumenda ut dolorum? Minus, officiis natus, aperiam voluptatibus mollitia doloribus pariatur, dignissimos quia nesciunt dolorum provident voluptates molest
+    """)
+        with open("storage/images/i.webp", "rb") as f:
+            st.download_button("Скачать косинуса", f, "i.webp")
+
+with memeTab:
+    st.write("ГыГы")
 
 
 st.badge("В разработке", icon=":material/code:", color="gray")
