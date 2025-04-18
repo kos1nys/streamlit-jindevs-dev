074161d658d423346f3562195ff8acd19bb31547
kos1nys <mrshark854@gmail.com>
2025-04-18 05:48:44 +0300

    1

--- pages/news/StartServer.py
+++ pages/news/StartServer.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.write("""
 *Название*, скоро запуск!
+         
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec luctus velit. Proin aliquam nunc ut elit aliquam, et pharetra elit pellentesque. Vestibulum eleifend quam at tortor fringilla, quis tincidunt purus hendrerit. Duis eget diam vel dolor hendrerit blandit. Ut sit amet velit eget nulla ultrices bibendum ut quis nisi. Donec vel lorem eu mi euismod consequat. In hac habitasse platea dictumst. Nullam eget ante at nisl bibendum aliquam. Praesent auctor quam sit amet
 """)
+
 
 st.badge("Новое", icon=":material/newspaper:", color="blue")
